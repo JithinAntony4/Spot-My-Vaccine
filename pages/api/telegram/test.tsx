1493b6eab08f8669f3b2c9b3b6dc22e3fc8e7cf3
Jithin Antony <jithinantony333@gmail.com>
2021-05-24 00:52:25 +0530

    telegram test api added

--- pages/api/telegram/test.tsx
+++ pages/api/telegram/test.tsx
@@ -5,7 +5,22 @@ export default async function telegramTest(req: NextApiRequest, res: NextApiResp
     try {
         let date = getCurrentFormattedDate();
         let district_id = 303;
-        let response = await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district_id}&date=${date}`);
+        let response = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=307&date=23-05-2021", {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9,ml;q=0.8",
+                "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site"
+            },
+            "referrer": "https://www.spotmyvaccine.in/",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
+            "mode": "cors"
+        });
         return res.send(`${response.status} ${await response.text()}`);
     } catch (e) {
         console.log(e.message);
