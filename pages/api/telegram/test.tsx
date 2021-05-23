2970500d38918a4650294966bf6da7a3c0c78fd0
Jithin Antony <jithinantony333@gmail.com>
2021-05-24 00:16:33 +0530

    telegram test api added

--- pages/api/telegram/test.tsx
+++ pages/api/telegram/test.tsx
@@ -6,7 +6,7 @@ export default async function telegramTest(req: NextApiRequest, res: NextApiResp
         let date = getCurrentFormattedDate();
         let district_id = 303;
         let response = await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district_id}&date=${date}`);
-        return res.send(await response.json());
+        return res.send(`${response.status} ${await response.text()}`);
     } catch (e) {
         console.log(e.message);
         return res.send(e.message);
