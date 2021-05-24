51091e437fe7b44d23d86005752fbb9475bd8c2c
Jithin Antony <jithinantony333@gmail.com>
2021-05-24 17:28:02 +0530

    added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,6 +41,19 @@ export default class MyDocument extends Document {
                     <meta property='og:site_name' content='SpotMyVaccine'/>
                     <meta property='og:url' content='https://spotmyvaccine.in/'/>
                     <meta property='og:image' content='/images/icon-512x512.png'/>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
+                        }}
+                    />
                 </Head>
                 <body>
                 <Main/>
